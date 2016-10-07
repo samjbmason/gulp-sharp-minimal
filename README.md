b993fbbede72ea920c24383c2127531474384f29
Sam Mason <samm@lostmy.name>
2016-10-07 11:55:17 +0100

    Update to working version

--- README.md
+++ README.md
@@ -1,15 +1,14 @@
-# gulp-sharp-minimal
+# lmn-gulp-svg-to-png
 
-Simple sharp plugin for gulp to modify images
+Simple sharp plugin for gulp to modify images, very specific to LMN's needs
 
 ## setup
 
 ### Linux
-Refer to official `sharp` docs [here](http://sharp.dimens.io/en/stable/install/#linux). For Linux environments the installation is super simple. 
+Refer to official `sharp` docs [here](http://sharp.dimens.io/en/stable/install/#linux). For Linux environments the installation is super simple.
   - `npm install`
 
 ### MacOS
 Refer to official `sharp` docs [here](http://sharp.dimens.io/en/stable/install/#mac-os)
   - `brew install homebrew/science/vips --with-webp`
   - `npm install`
-  
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict';
 var through = require('through2');
 var gutil = require('gulp-util');
-var getFileSize = require("filesize");
 var sharp = require('sharp');
 
 const PLUGIN_NAME = 'gulp-sharp-minimal';
@@ -9,7 +8,6 @@ const PLUGIN_NAME = 'gulp-sharp-minimal';
 function gulpSharpMinimal(options){
 
   return through.obj(function(file, encoding, callback) {
-
 
     if (file.isNull()) {
       this.push(file)
@@ -22,8 +20,8 @@ function gulpSharpMinimal(options){
     }
 
     if (file.isBuffer()) {
-      // this.emit('error', new gutil.PluginError(PLUGIN_NAME, "Received a buffer..."));
       var image = sharp(file.contents);
+      var format = !options.format ? metadata.format : options.format
 
       image
         .metadata()
@@ -33,47 +31,19 @@ function gulpSharpMinimal(options){
             .withoutEnlargement()
             .quality((!options.quality ? 80 : options.quality))
             .compressionLevel((!options.compressionLevel ? 6 : options.compressionLevel))
-            .toFormat((!options.format ? metadata.format : options.format))
-            .toBuffer()
-        })
-        .then(function(data){
-          if (options.progressive){
-            image.progressive()
-          }
-
-          if (options.stripMetadata){
-            // if true - then we keep all EXIF data of image
-            // otherwise default behavior is to strip it all
-            image.withMetadata();
-          }
-          return image;
         })
 
-        .then(function(sequentialRead){
-          if (options.sequentialRead){
-            image.sequentialRead()
-          }
-          return image;
-
-         })
-
-        .then(function(trellisQuantisation){
-          if (options.trellisQuantisation){
-            image.trellisQuantisation()
-          }
-          return image;
-        })
-
-        .then(function(data) {
-
+      image
+        .toFormat(format)
+        .toBuffer(function(err, buffer) {
           var newFile = new gutil.File({
             cwd: file.cwd,
             base: file.base,
-            path: file.path,
+            path: gutil.replaceExtension(file.path, '.' + format),
             contents: image
           });
-          // gutil.log(PLUGIN_NAME + ':', gutil.colors.green(newFile.relative + " : size before: "+file_size_before+" : size after: "+file_size_after));
-          callback(null, data);
+
+          callback(null, newFile);
       });
     }
   });
